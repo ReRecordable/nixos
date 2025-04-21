0d1fb7648ba874fcc09581c53253e864629b0415
ReRecordable <vehicularmanslaughter0@gmail.com>
2025-04-21 19:24:51 -0400

    Add the surface

--- flake.nix
+++ flake.nix
@@ -5,9 +5,12 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     jovian.url = "github:Jovian-Experiments/Jovian-NixOS";
     jovian.inputs.nixpkgs.follows = "nixpkgs";
+    nvf.url = "github:NotAShelf/nvf";
+    nvf.inputs.nixpkgs.follows = "nixpkgs";
+    zen.url = "github:MarceColl/zen-browser-flake";
   };
 
-  outputs = {self, nixpkgs, jovian, ... }@inputs: { 
+  outputs = {self, nixpkgs, jovian, nvf, ... }@inputs: { 
     nixosConfigurations = {
       # Disable this for now, going to redo the config.
       # "nix-deck" = nixpkgs.lib.nixosSystem {
@@ -19,6 +22,11 @@
         system = "x86_64-linux";
 	specialArgs = { inherit inputs; };
 	modules = [ ./Devices/5590/configuration.nix ];
+      };
+      "nixos" = nixpkgs.lib.nixosSystem { 
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [ ./Devices/surface/configuration.nix nvf.nixosModules.default ];
       };
     };
   };
